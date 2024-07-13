d990e8664ade40f3e93307d65e6b2a192027f1c1
Paula Bigalke <paula.bigalke@uni-koeln.de>
2024-07-13 12:27:09 +0200

    include reading of raw MWCCH files in preprocessing script

--- preprocess_data/preprocess_MWCC_H.py
+++ preprocess_data/preprocess_MWCC_H.py
@@ -1,9 +1,77 @@
+# %%
+import os
+import glob
+import xarray as xr
+import pandas as pd
+import sys
+sys.path.append("..")
+# import my own script
+import helpers.helper_conversions as hlp
+from config.domain_info import domain_expats
 
 # %%
-import pandas as pd
-from datetime import datetime
-import glob
+def main():
+    path = "/net/merisi/pbigalke/data/MWCC-H/H2MED_data"
+    output_path = "/net/merisi/pbigalke/data/MWCC-H/netcdf"
+    # years = [2022]
+    # months = [6]
+    # days = [5]
+    # detectors = ["ATMS", "MHS", "SSMIS", "GMI"]
+    # all_files = _get_mwcch_files_in_study_period(path, detectors, years, months, days)
+    # print(len(all_files))
+    # count = 0
 
+    # read all files in directory
+    all_files = sorted(glob.glob(f"{path}/*/*/*.asc.gz"))
+
+    count = 0
+    count_in_domain = 0
+    # loop over files
+    for f, fl in enumerate(all_files):
+
+        # print status every few files
+        if f % 1000 == 0:
+            print(f"{count}/{len(all_files)}: {count_in_domain} files within domain", flush=True)
+
+        if save_mwcch_over_domain_as_netcdf(fl, domain_expats, output_path):
+            count_in_domain += 1
+
+        count += 1
+
+#%%
+def save_mwcch_over_domain_as_netcdf(mwcch_file, domain, output_path):
+    
+    # read in data file
+    data = _read_mwcch_file(mwcch_file, domain=domain)
+
+    if len(data) > 0:
+        # get start and end datetime
+        start_dt, end_dt = data['datetime'].agg(['min', 'max'])
+
+        # get detector from filename
+        detector = _get_detector_from_mwcch_filepath(mwcch_file)
+
+        # get satellite name from filename
+        satellite = _get_satellite_from_mwcch_filepath(mwcch_file)
+
+        # get date string from start datetime
+        date_string = hlp.get_datestring_from_npdatetime(start_dt)
+
+        # get starting and end time within our domain
+        start_time = f"S{hlp.get_timestring_from_npdatetime(start_dt)}"
+        end_time = f"E{hlp.get_timestring_from_npdatetime(end_dt)}"
+        
+        # define netcdf file name
+        netcdf_path = f"{output_path}/{date_string[:4]}/{date_string[4:6]}/{date_string[6:]}"
+        if not os.path.exists(netcdf_path):
+            os.makedirs(netcdf_path)
+        netcdf_file = f"{netcdf_path}/{date_string}_{start_time}_{end_time}_{detector}_{satellite}.nc"
+
+        # save as netcdf file
+        data_xr = xr.Dataset.from_dataframe(data)
+        data_xr.to_netcdf(netcdf_file)
+        return True
+    return False
 
 # %%
 def _crop_over_domain(data, domain):
@@ -28,7 +96,6 @@ def _crop_over_domain(data, domain):
     data.drop(index_out_of_bounds, inplace=True)
     return data
     
-
 def _get_column_names(detector):
     """ read column names depending on satellite
 
@@ -54,8 +121,7 @@ def _get_column_names(detector):
     elif detector == "SSMIS":
         return ['year', 'month', 'day', 'hour', 'min', 'sec', 'lat', 'lon', 'cloud_type', 'tb_150', 'POH']
 
-
-def read_mwcch_file_old(file_path, domain=None):
+def _read_mwcch_file_old(file_path, domain=None):
     """ read MWCC-H output containing probability of hail into dataframe
 
     Parameters
@@ -93,7 +159,7 @@ def read_mwcch_file_old(file_path, domain=None):
     #         value =  df.apply(lambda x : datetime(int(x['year']), int(x['month']), int(x['day']), int(x['hour']), int(x['min']), int(x['sec'])), axis=1))
     return df
 
-def read_mwcch_file(file_path, domain=None):
+def _read_mwcch_file(file_path, domain=None):
     """ read MWCC-H output containing probability of hail into dataframe
 
     Parameters
@@ -130,7 +196,7 @@ def read_mwcch_file(file_path, domain=None):
     
     return df
 
-def get_y_m_d_from_mwcch_filepath(file_path):
+def _get_y_m_d_from_mwcch_filepath(file_path):
     # get year from path
     year = file_path.split('/')[-3]
 
@@ -146,7 +212,7 @@ def get_y_m_d_from_mwcch_filepath(file_path):
 
     return year, month, day
 
-def get_satellite_from_mwcch_filepath(file_path):
+def _get_satellite_from_mwcch_filepath(file_path):
     satellites = ['meto01', 'meto02', 'meto03', 'noaa15', 'noaa16', 'noaa17', 'noaa18', 'noaa19', 
                   'n20', 'n21', 'npp', 'f16', 'f17', 'gpm']
     for sat in satellites:
@@ -154,10 +220,10 @@ def get_satellite_from_mwcch_filepath(file_path):
             return sat
     return None
 
-def get_detector_from_mwcch_filepath(file_path):
+def _get_detector_from_mwcch_filepath(file_path):
     return file_path.split('/')[-2]
 
-def get_mwcch_files_in_study_period(mwcch_directory, detectors, years, months=None, days=None):
+def _get_mwcch_files_in_study_period(mwcch_directory, detectors, years, months=None, days=None):
     
     if detectors is not None and not isinstance(detectors, list):
         detectors = list(detectors)
@@ -175,7 +241,7 @@ def get_mwcch_files_in_study_period(mwcch_directory, detectors, years, months=No
             for f in glob.glob(f"{mwcch_directory}/{year}/{detector}/*.asc.gz"):
                 #mwcch_files.append(f)
                 try:
-                    year, month, day = get_y_m_d_from_mwcch_filepath(f)
+                    year, month, day = _get_y_m_d_from_mwcch_filepath(f)
                 except ValueError:
                     print("error when retrieving date string from file path: ", f)
                 
@@ -186,19 +252,7 @@ def get_mwcch_files_in_study_period(mwcch_directory, detectors, years, months=No
     return mwcch_files
 
 # %%
-if __name__ == '__main__':
-    import numpy as np
-    # test on exaple file
-    example_files = ["/net/merisi/pbigalke/data/MWCC-H/H2MED_data/2023/ATMS/atms_20231003-S1123-E1305_npp_061830.asc.gz", 
-                     "/net/merisi/pbigalke/data/MWCC-H/H2MED_data/2023/GMI/gmi_20230731-S2032-E2205_gpm_053535.asc.gz",
-                     "/net/merisi/pbigalke/data/MWCC-H/H2MED_data/2023/MHS/BT_rain_proxy_202309041856_noaa19_75119.asc.gz",
-                     "/net/merisi/pbigalke/data/MWCC-H/H2MED_data/2023/SSMIS/ssmis_20230910-S0241-E0423_f17_086947.asc.gz"]
-
-    domain = {"minlon":5., "maxlon":16., "minlat":42., "maxlat":51.5}
-    for f in example_files:
-        print('-----------------------------------------------------')
-        print(f)
-        data = read_mwcch_file(f, domain=domain)
-        print(data)
+if __name__ == "__main__":
+    main()
 
 # %%
