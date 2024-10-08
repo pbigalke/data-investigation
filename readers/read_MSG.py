fc194b23e9b2ab27d100b55d036a62a14bbbba8a
Paula Bigalke <paula.bigalke@uni-koeln.de>
2024-10-08 10:20:47 +0200

    add docstrings - copilot rocks!!!

--- readers/read_MSG.py
+++ readers/read_MSG.py
@@ -12,17 +12,38 @@ CHANNELS = ["IR_016", "IR_039", "IR_087", "IR_097", "IR_108", "IR_120", "IR_134"
 
 # %%
 def read(msg_file, channels=None):
+    """Read MSG file and return xarray dataset
 
+    Args:
+        msg_file (pathlike): path to MSG file
+        channels (list(str), optional): list of channel names that should be read in. Defaults to None.
+
+    Returns:
+        xr.dataset: xarray dataset containing MSG data
+    """
     drop = [ch for ch in CHANNELS if ch not in channels] if channels is not None else None
     with xr.open_dataset(msg_file, drop_variables=drop) as dataset:
         return dataset
 
 def get_y_m_d_from_filepath(msg_file):
+    """Extract year, month and day from MSG file path
+
+    Args:
+        msg_file (pathlike): path to MSG file
+
+    Returns:
+        str, str, str: year, month, day
+    """
     name = os.path.basename(msg_file)
     date = name.split('_')[0]
     return date[:4], date[4:6], date[6:]
 
 def get_lon_lat():
+    """Get longitude and latitude values of regular gridded MSG files given in MSG_PATH
+
+    Returns:
+        np.array(float), np.array(float): longitude and latitude values
+    """
     MSG_example_file = f"{MSG_PATH}/2023/09/20230930-EXPATS-RG.nc"
     with xr.open_dataset(MSG_example_file, drop_variables=CHANNELS) as dataset:
         lon = dataset.lon.values
@@ -30,6 +51,14 @@ def get_lon_lat():
     return lon, lat
 
 def get_MSG_files_from_timestamps(msg_dt):
+    """Get MSG files from timestamps
+
+    Args:
+        msg_dt (np.datetime64): timestamps (list or single timestamp)
+
+    Returns:
+        pathlike: corresponding of MSG files (list or single path)
+    """
     # check if input is list
     if not isinstance(msg_dt, list):
         msg_dt = [msg_dt]
