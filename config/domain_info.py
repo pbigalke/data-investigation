baa1f3e2a30b90de81a4c077ec87a607d8b01f21
Paula Bigalke <paula.bigalke@uni-koeln.de>
2024-10-18 19:45:24 +0200

    add domain for hail information

--- config/domain_info.py
+++ config/domain_info.py
@@ -7,6 +7,7 @@ figure and domain infos
 domain_dfg          =  [10.75, 11.75,  45.5,  47.  ] # minlon, maxlon, minlat, maxlat
 domain_German_flood =  [ 5.,    9.,    48.,   52.  ] # minlon, maxlon, minlat, maxlat
 domain_expats       =  [ 5.,   16.,    42.,   51.5 ] # minlon, maxlon, minlat, maxlat
+domain_expats_hail  =  [ 5.,   16.,    42.,   50 ] # minlon, maxlon, minlat, maxlat
 
 
 #domain_DE_CA        =  [ 8.3,  13.,    47.5,  49.5]  # minlon, maxlon, minlat, maxlat
