bc80b4054b7274283d0592bcb56dc546d5fd1f1f
Paula Bigalke <paula.bigalke@uni-koeln.de>
2024-10-21 13:44:56 +0200

    plot for 2 example days

--- constructing_dataset/plot_cropping_methods.py
+++ constructing_dataset/plot_cropping_methods.py
@@ -8,7 +8,7 @@ from config. domain_info import domain_expats, domain_expats_hail
 import readers.read_processed_MWCC_H as mwcch_read
 import matching_data.collect_matching_files as match
 import helpers.datetime_helper as hlp
-import constructing_dataset.crop_time_series_over_hail as tscrop
+import constructing_dataset.crop_over_hail_or_overpass as cropover
 import plotting.plot_MWCC_H as mwcc_plt
 
 c_hail_area = "cyan"
@@ -18,7 +18,8 @@ c_ot = "red"
 
 # %%
 def plot_different_crop_positions(msg_timestamp_data, mwcch_data, cropsize, min_pixel, output_path, 
-                                  crops=["maxhailarea"], recenter=False, mwcch_mode="hail_class"):
+                                  crops=["maxhailarea"], recenter=False, mwcch_mode="hail_class", 
+                                  domain=domain_expats_hail):
 
     # get area coverage
     overpass_area = mwcch_read.area_percentage_covered_by_overpass(mwcch_data.hail_class.values)
@@ -26,40 +27,41 @@ def plot_different_crop_positions(msg_timestamp_data, mwcch_data, cropsize, min_
     mark_points=[]
     draw_subdomains=[]
 
-    for crop in crops:
-        if crop == "maxhailarea":
-            try:
-                # get crop extent over ------------------------------------------------------------------- max hail area
-                cg_lon, cg_lat, minlon, maxlon, minlat, maxlat = \
-                    tscrop.get_crop_extent_over_maxhailarea(mwcch_data, cropsize, min_pixel=min_pixel)
-            except TypeError:
-                print("ERROR: crop extent could not be calculated.")
-                continue
+    if crops is not None:
+        for crop in crops:
+            if crop == "maxhailarea":
+                try:
+                    # get crop extent over ------------------------------------------------------------------- max hail area
+                    cg_lon, cg_lat, minlon, maxlon, minlat, maxlat = \
+                        cropover.get_crop_extent_over_maxhailarea(mwcch_data, cropsize, min_pixel=min_pixel)
+                except TypeError:
+                    print("ERROR: crop extent could not be calculated.")
+                    continue
 
-            mark_points.append([cg_lon, cg_lat, c_hail_area, 'x'])
-            draw_subdomains.append([minlon, maxlon, minlat, maxlat, c_hail_area, '-'])
+                mark_points.append([cg_lon, cg_lat, c_hail_area, 'x'])
+                draw_subdomains.append([minlon, maxlon, minlat, maxlat, c_hail_area, '-'])
 
-            if recenter:
-                # get crop extent over ------------------------------------------------------------------- diff WV-IR within crop
-                cg_lon_diff, cg_lat_diff, minlon_diff, maxlon_diff, minlat_diff, maxlat_diff = \
-                    tscrop.recenter_crop_over_highest_clouds(msg_timestamp_data, [minlon, maxlon, minlat, maxlat])
+                if recenter:
+                    # get crop extent over ------------------------------------------------------------------- diff WV-IR within crop
+                    cg_lon_diff, cg_lat_diff, minlon_diff, maxlon_diff, minlat_diff, maxlat_diff = \
+                        cropover.recenter_crop_over_highest_clouds(msg_timestamp_data, [minlon, maxlon, minlat, maxlat])
 
-                # get crop extent over ------------------------------------------------------------------- OT area within crop
-                cg_lon_OT, cg_lat_OT, minlon_OT, maxlon_OT, minlat_OT, maxlat_OT = \
-                    tscrop.recenter_crop_over_highest_clouds(msg_timestamp_data, [minlon, maxlon, minlat, maxlat], mode='OT')
+                    # get crop extent over ------------------------------------------------------------------- OT area within crop
+                    cg_lon_OT, cg_lat_OT, minlon_OT, maxlon_OT, minlat_OT, maxlat_OT = \
+                        cropover.recenter_crop_over_highest_clouds(msg_timestamp_data, [minlon, maxlon, minlat, maxlat], mode='OT')
 
-                mark_points.extend([[cg_lon_diff, cg_lat_diff, c_diffWVIR, 'x'], 
-                                    [cg_lon_OT, cg_lat_OT, c_ot, 'x']])
-                draw_subdomains.extend([[minlon_diff, maxlon_diff, minlat_diff, maxlat_diff, c_diffWVIR, '-'],
-                                        [minlon_OT, maxlon_OT, minlat_OT, maxlat_OT, c_ot, '-']])
+                    mark_points.extend([[cg_lon_diff, cg_lat_diff, c_diffWVIR, 'x'], 
+                                        [cg_lon_OT, cg_lat_OT, c_ot, 'x']])
+                    draw_subdomains.extend([[minlon_diff, maxlon_diff, minlat_diff, maxlat_diff, c_diffWVIR, '-'],
+                                            [minlon_OT, maxlon_OT, minlat_OT, maxlat_OT, c_ot, '-']])
 
-                
-        elif crop == "overpassarea":
-            # get crop extent over ------------------------------------------------------------------- overpass area
-            cg_lon_over, cg_lat_over, minlon_over, maxlon_over, minlat_over, maxlat_over = \
-                tscrop.get_crop_extent_over_overpassarea(mwcch_data, cropsize)
-            mark_points.append([cg_lon_over, cg_lat_over, c_over, 'x'])
-            draw_subdomains.append([minlon_over, maxlon_over, minlat_over, maxlat_over, c_over, '-'])
+                    
+            elif crop == "overpassarea":
+                # get crop extent over ------------------------------------------------------------------- overpass area
+                cg_lon_over, cg_lat_over, minlon_over, maxlon_over, minlat_over, maxlat_over = \
+                    cropover.get_crop_extent_over_overpassarea(mwcch_data, cropsize)
+                mark_points.append([cg_lon_over, cg_lat_over, c_over, 'x'])
+                draw_subdomains.append([minlon_over, maxlon_over, minlat_over, maxlat_over, c_over, '-'])
 
 
     # ---------------------------------------------------------------------
@@ -80,16 +82,18 @@ def plot_different_crop_positions(msg_timestamp_data, mwcch_data, cropsize, min_
         # plot last timestamp with hail area crop
         dt = hlp.get_datetimestring_from_npdatetime(msg_timestamp_data.time.values)
         title_str = f"{dt[:4]}-{dt[4:6]}-{dt[6:8]} {dt[-4:-2]}:{dt[-2:]}, coverage: {overpass_area} %"
-        outname = f"{output_path}/{dt}_{channel}_{mwcch_mode}_crop_over" + "".join([f"_{crop}" for crop in crops])
-        if recenter:
-            outname += "_recentered"
+        outname = f"{output_path}/{dt}_{channel}_{mwcch_mode}_{domain[0]}-{domain[1]}_{domain[2]}-{domain[3]}"  
+        if crops is not None: 
+            outname += "_crop_over" + "".join([f"_{crop}" for crop in crops])
+            if recenter:
+                outname += "_recentered"
 
         # plot crops over MSG and MWCC-H
         mwcc_plt.plot_mwcch_over_MSG(msg_timestamp_data.lon.values, msg_timestamp_data.lat.values, msg_tb.values, channel, 
                                     mwcch_data.lon.values, mwcch_data.lat.values, mwcch_data.POH.values, mwcch_mode=mwcch_mode,
                                     mark_points=mark_points, draw_subdomains=draw_subdomains, 
                                     vmin=channels[channel]["vmin"], vmax=channels[channel]["vmax"], 
-                                    domain=domain_expats, title=title_str, path_out=None) #f"{outname}.png")
+                                    domain=domain, title=title_str, path_out=f"{outname}.png")
 
 
 
@@ -106,7 +110,7 @@ if __name__ == "__main__":
 
     years = [2022]
     months = [6]
-    days = [5]
+    days = [5, 20]
     msg_res = 15
     cropsize = 128
     min_pixel = 1
@@ -123,7 +127,7 @@ if __name__ == "__main__":
     print(len(mwcch_files))
 
     # loop over mwcch files
-    for file in mwcch_files[:3]:
+    for file in mwcch_files:
         print()
         print(os.path.basename(file))
 
@@ -141,7 +145,11 @@ if __name__ == "__main__":
         # read in MSG data and filter for timestamp
         msg_timestamp_data = msg_read.read(msg_file, channels=unique_channels).sel(time=msg_timestamp)
 
-        # plot_different_crop_positions(msg_path, mwcch_path, output_path, years, months, days, msg_res)
-        plot_different_crop_positions(msg_timestamp_data, mwcch_data, cropsize, min_pixel, output_path, 
-                                      crops=["maxhailarea", "overpassarea"], mwcch_mode="hail_class")
+        for domain in [domain_expats_hail, domain_expats]:
+            
+            # plot_different_crop_positions(msg_path, mwcch_path, output_path, years, months, days, msg_res)
+            plot_different_crop_positions(msg_timestamp_data, mwcch_data, cropsize, min_pixel, output_path, 
+                                        domain=domain, crops=["maxhailarea", "overpassarea"], mwcch_mode="hail_class") # crops=["maxhailarea", "overpassarea"]
+            plot_different_crop_positions(msg_timestamp_data, mwcch_data, cropsize, min_pixel, output_path, 
+                                        domain=domain, crops=None, mwcch_mode="hail_class") # crops=["maxhailarea", "overpassarea"]
 # %%
